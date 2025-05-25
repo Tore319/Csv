cf428eb7717fcff01b8ff0b9c47937369eb04bcb
Juanjo <juanjotr31@gmail.com>
2025-05-25 12:04:29 +0200

    fix: migration

--- database/seeders/CsvSeeder.php
+++ database/seeders/CsvSeeder.php
@@ -13,14 +13,14 @@ class CsvSeeder extends Seeder
      */
     public function run(): void
     {
-        $csv1 = new Csv();
-        $csv1->hash = 'dee004a8ac7d653e1e581513d0f8227833404e8a';
-        $csv1->csv = 'IMEdee004a8ac7d653e1e581513d0f8227833404e8a10000014';
-        $csv1->DNI = '20947867L';
-        $csv1->nombre = 'Juanjo';
-        $csv1->apellidos = 'Torres Roig';
-        $csv1->correo = 'juanjo@gmail.com';
-        $csv1->archivo = 'csv/LWe0mq5lVt5FDKU5nKzcyStrlSco5bM6QNNANkMX.pdf';
-        $csv1->save();
+        // $csv1 = new Csv();
+        // $csv1->hash = 'dee004a8ac7d653e1e581513d0f8227833404e8a';
+        // $csv1->csv = 'IMEdee004a8ac7d653e1e581513d0f8227833404e8a10000014';
+        // $csv1->DNI = '20947867L';
+        // $csv1->nombre = 'Juanjo';
+        // $csv1->apellidos = 'Torres Roig';
+        // $csv1->correo = 'juanjo@gmail.com';
+        // $csv1->archivo = 'csv/LWe0mq5lVt5FDKU5nKzcyStrlSco5bM6QNNANkMX.pdf';
+        // $csv1->save();
     }
 }
