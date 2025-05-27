956d601ab1369431540ad8ba114ba42d7f64d7d4
Juanjo <juanjotr31@gmail.com>
2025-05-27 10:28:24 +0200

    fix: button to show and download

--- resources/views/csv/show.blade.php
+++ resources/views/csv/show.blade.php
@@ -8,6 +8,11 @@
             <div class="d-flex justify-content-around mb-3 p-3 border bg-light mx-auto" style="width: 45%;">
                 <embed src="{{asset('storage/' . $sql->archivo)}}" type="application/pdf" width="75%" height="500px"/>
             </div>
+            <div class="d-flex justify-content-center align-items-center" style="height: 50px;">
+                <a href="{{ asset('storage/' . $sql->archivo) }}" target="_blank" class="btn btn-success" style="margin-top: 20px; margin-left: 10px;">
+                    <i class="bi bi-file-earmark"></i>
+                </a>
+            </div>
         </div>
     @endif
 @endsection
