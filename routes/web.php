73c9088df09a6ee4f3e8df424ba310190c5a7940
Juanjo <juanjotr31@gmail.com>
2025-06-02 17:35:20 +0200

    Revert "fix: routes"
    This reverts commit 7745ac8b008bcb0c2584063014d0a20493a7fe4b.

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,19 @@ Route::get('/', function () {
     return view('inicio');
 })->name('inicio');
 
+Route::get('/csv/search', [CsvController::class, 'search'])->name('search');
+//Route::post('/csv/download', [CsvController::class, 'download'])->name('download');
 Route::post('/csv/show', [CsvController::class, 'show'])->name('show');
+Route::resource('/csv',CsvController::class);
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/csv/search', [CsvController::class, 'search'])->name('search');
-    Route::resource('/csv',CsvController::class);
-    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
